cc03b399e8493d4efae199ebb24803d7a6cfcc81
DuncanBScruggs <duncanbscruggs@gmail.com>
2020-12-08 21:40:13 -0500

    Ian is a gentleman and a scholar

--- src/Components/Game.js
+++ src/Components/Game.js
@@ -13,7 +13,7 @@ function Game() {
     const [activeCharacter, setActiveCharacter] = useState([]);
     const [locationsList, setLocationsList] = useState([]);
     const [ifChecked, setIfChecked] = useState([]);
-    
+
 
     function clickHandler() {
         const method = 'post';
@@ -42,8 +42,12 @@ function Game() {
         if (res.status == 200) {
             console.log(res);
             setCharactersList(res.data)
+            if(res.data.length>0){
+                setActiveCharacter(res.data[0])
+            }
         }
     }
+    
     function getCharactersTasks(res) {
         if (res.status == 200) {
             console.log(res);
@@ -58,43 +62,42 @@ function Game() {
         }
     }
 
-    function updateTask(taskId) {
-        if (taskId == ifChecked){
-            const method = 'delete';
+    function updateTask(taskObj, foundCharacterTask) {
+        if (foundCharacterTask){
+            const method = 'post';
             const route = 'deleteCharacterTask';
-            const data = { ref_task_id: taskId, ref_character_id: activeCharacter[0].id }
-            
-            AxiosHelper({ method, route, data })   
+            const data = { id: taskObj.id, ref_game_id: params.gameid  }
+            AxiosHelper({ method, route, data, fun:getLocations, token })
         }
         else{
-            setIfChecked(taskId)
+            setIfChecked(taskObj.id)
             const method = 'post';
             const route = 'createCharacterTask';
-            const data = { ref_task_id: taskId, ref_character_id: activeCharacter[0].id }
-            
-            AxiosHelper({ method, route, data })     
+            const data = { ref_task_id: taskObj.id, ref_character_id: activeCharacter[0].id, ref_game_id: params.gameid  }
+            AxiosHelper({ method, route, data, fun:getLocations, token })
         }
     }
 
     useEffect(() => {
+        if (charactersList.length > 0) {
+            setActiveCharacter(charactersList[0])
+        }
+    }, [charactersList.length])
 
+
+
+    useEffect(() => {
         const ssToken = window.sessionStorage.getItem('token')
         if (ssToken) {
             const method = 'post'
             const route = 'userCharactersIndex'
             const data = { ref_game_id: params.gameid }
-
             console.log(params.gameid)
-
             AxiosHelper({ method, route, token: ssToken, fun: getCharactersList, data })
-
-
             // if (charactersList > 0 && user.account_verified_at.length < 0) {
-
             // }
             // else if( user has made no character && user.account_verified_at < 0 ){
             // else if (charactersList < 0 && user.account_verified_at.length < 0) {
-
             // }
             // user is not verified but have no characters
             // }
@@ -105,16 +108,13 @@ function Game() {
             AxiosHelper({ method, route, fun: unauthToken, })
             // save token of fake user
         }
-
-
         // const method = 'get';
         // const route = `getLocations/${params.gameid}`;
         AxiosHelper({ method:'get', route: `getLocations/${params.gameid}`, fun: getLocations })
-
         // AxiosHelper({ method:'get', route: 'getCharacterTask', fun: getCharactersTasks })
+    }, [token])
 
 
-    }, [])
 
     return (
         <>
@@ -128,7 +128,7 @@ function Game() {
             </div>
             <button onClick={clickHandler} class="btn btn-primary">Create Character</button>
 
-            <div class="dropdown">
+            <div class="dropdown my-5">
                 <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenu2" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                     Select Character
                 </button>
@@ -148,20 +148,23 @@ function Game() {
                 return (
                     <>
                         <h2>{item.location}</h2>
-                        {item.tasks.map((task) => {
+                        {activeCharacter.length > 0 && item.tasks.map((task) => {
+                            const found = task.charactertask.find(c_t=>c_t.ref_character_id === activeCharacter[0].id)
+                            const renderCheck = found === undefined ? "checked" : ""
                             return (
-
                                 <div  key={task.id} class="form-check">
-                                    <input onClick={() => updateTask(task.id)} class="form-check-input" type="checkbox" value="" id="defaultCheck1" />
+                                    <input onClick={() => updateTask(task, found)} class="form-check-input" type="checkbox" value={renderCheck} id="defaultCheck1" />
                                     <label class="form-check-label" for="defaultCheck1" >
                                         {task.name}
-                                    </label> 
+                                    </label>
                                 </div>
                             )
                         })}
                     </>
                 )
             })}
+
+
 
 
         </>
